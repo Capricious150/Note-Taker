5825a8a7846eae8c3f03a7fc45e19650c1e6b565
Austin A <austinandrews89@gmail.com>
2022-03-11 17:29:38 -0700

    Updated PORT to 8080

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const PORT = 3001;
+const PORT = 8080;
 const app = express();
 const path = require('path');
 const utilities = require('./utils/utilities.js');
