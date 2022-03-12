be408bb836c9b4c40d73ffea69aa3da5ed75fba8
Austin A <austinandrews89@gmail.com>
2022-03-11 17:35:04 -0700

    Updated port to .listen(process.env.PORT || 5000)

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const PORT = 8080;
+const PORT = (process.env.PORT || 5000);
 const app = express();
 const path = require('path');
 const utilities = require('./utils/utilities.js');
